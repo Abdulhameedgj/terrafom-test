2fb6ff534a84fba27644aac148091f6b80a91158
Abdulhameedgj <abdulhameedgj@gmail.com>
2024-04-16 20:13:21 +0530

    Add files via upload

---
+++ vpc.tf
@@ -0,0 +1,47 @@
+module "label_vpc" {
+  source     = "cloudposse/label/null"
+  version    = "0.25.0"
+  context    = module.base_label.context
+  name       = "vpc"
+  attributes = ["main"]
+}
+
+resource "aws_vpc" "main" {
+  cidr_block           = var.vpc_cidr
+  enable_dns_support   = true
+  enable_dns_hostnames = true
+  tags                 = module.label_vpc.tags
+}
+
+resource "aws_subnet" "public_subnet" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = cidrsubnet(var.vpc_cidr, 8, 1)
+  availability_zone = var.availability_zone
+}
+
+resource "aws_subnet" "private_subnet" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = cidrsubnet(var.vpc_cidr, 8, 2)
+  availability_zone = var.availability_zone
+}
+
+resource "aws_internet_gateway" "my_igw" {
+  vpc_id = aws_vpc.main.id
+}
+
+resource "aws_route_table" "public_subnet_route_table" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.my_igw.id
+  }
+
+  
+}
+
+resource "aws_route_table_association" "public_subnet_association" {
+  subnet_id      = aws_subnet.public_subnet.id
+  route_table_id = aws_route_table.public_subnet_route_table.id
+}
+
