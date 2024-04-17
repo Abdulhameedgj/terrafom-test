3dabdff48f67f853bccfdcd839a91756db7947f7
Abdulhameedgj <abdulhameedgj@gmail.com>
2024-04-17 15:04:16 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -9,10 +9,10 @@ variable "aws_region" {
    type        = string
  }
 
- variable "aws_secret_key" {
-   description = "AWS Secret Key for the target AWS account"
-   type        = string
- }
+# variable "aws_secret_key" {
+#   description = "AWS Secret Key for the target AWS account"
+#   type        = string
+# }
 
  variable "aws_session_token" {
    description = "AWS Session Token for the target AWS account. Required only if authenticating using temporary credentials"
